8ab03c0de3cd7fd98f0050672178718cd8eecb64
Daniel Dayton <danieldayton33@gmail.com>
2026-02-26 21:16:04 -0500

    email signup

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,21 +1,7 @@
 'use server';
 
 import { signIn } from '@/auth/config';
-import { redirect } from 'next/navigation';
 
-export async function signInWithCredentials(email: string, password: string) {
-  const result = await signIn('credentials', {
-    email,
-    password,
-    redirect: false,
-  });
-  console.log('result signInWithCredentials', result);
-
-  if (result?.error) {
-    return { error: result.error };
-  }
-  return result;
-}
 
 export async function signInWithGoogle() {
   await signIn('google', {
@@ -24,6 +10,7 @@ export async function signInWithGoogle() {
 }
 
 export async function signInWithPostmark(email: string) {
+ 
   const isDev = process.env.NODE_ENV !== 'production';
   if (isDev) {
     return await signIn('dev-email', {
